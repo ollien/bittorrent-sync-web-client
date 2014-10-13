4cf428e90f420130edcbd489e67c706d057c3971
Nick Krichevsky <njk828@gmail.com>
2014-10-12 22:37:19 -0400

    Fix bug that would allow 404s to happen when going into subfolders

--- static/js/filelist.js
+++ static/js/filelist.js
@@ -261,6 +261,9 @@ String.prototype.repeat = function(num)
 	return new Array( num + 1 ).join(this);
 }
 $(document).ready(function(){
+	if (window.location.href[window.location.href.length-1]!='/'){
+		window.location.href+='/';
+	}
 	if (!isRoot()){
 		$('#addSecret').hide();
 	}
